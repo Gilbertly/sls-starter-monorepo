15c7479c2adbf576e80f6a1868427cab286de599
Gilbertly <gilbert@qubitping.com>
2021-04-24 14:42:50 +0300

    refactor: add lerna config

---
+++ services/auth/src/functions/handler.js
@@ -0,0 +1,9 @@
+import sample1 from 'sample1';
+import lib from '../../../../libs';
+
+export async function handler(event, context) {
+  return {
+    statusCode: 200,
+    body: `Hello World! Via auth service!`,
+  };
+}
---
+++ services/search/src/functions/handler.js
@@ -0,0 +1,10 @@
+import * as Sentry from '@sentry/node';
+
+Sentry.init({ dsn: process.env.SENTRY_DSN || '' });
+
+export async function handler(event, context) {
+  return {
+    statusCode: 200,
+    body: `Hello World! Via search service!`,
+  };
+}
